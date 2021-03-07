38449b2431640dd6c01d5ca5964be60129196c0a
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-03-07 23:28:06 +0530

    Routes for Editing Comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router({mergeParams: true});
+var router = express.Router({ mergeParams: true });
 var Campground = require('../models/campground');
 var Comment = require('../models/comment');
 
@@ -25,13 +25,13 @@ router.post('/', isLoggedIn, (req, res) => {
             console.log(err);
             res.redirect('/campgrounds');
         }
-        else{
+        else {
             var text = req.body.text;
             var author = {
                 id: req.user._id,
                 username: req.user.username,
-            } ; 
-            var tempComment = {text : text, author : author};
+            };
+            var tempComment = { text: text, author: author };
             Comment.create(tempComment, (err, comment) => {
                 if (err)
                     console.log(err);
@@ -43,6 +43,25 @@ router.post('/', isLoggedIn, (req, res) => {
             });
         }
     });
+});
+
+
+router.get('/:comment_id/edit', (req, res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if (err)
+            res.redirect('back');
+        else
+            res.render('comments/edit', { campground_id: req.params.id, comment: foundComment });
+    });
+});
+
+router.put('/:comment_id', (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+        if(err)
+            res.redirect('back');
+        else
+            res.redirect('/campgrounds/' + req.params.id);
+    })
 });
 
 module.exports = router;
