1aa39f0591fedb11817588a24382002299b88f0c
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-03-08 23:48:44 +0530

    Comment Deletion

--- routes/comments.js
+++ routes/comments.js
@@ -64,4 +64,13 @@ router.put('/:comment_id', (req, res) => {
     })
 });
 
+router.delete('/:comment_id', (req, res) => {
+    Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+        if(err)
+            res.redirect('back');
+        else
+            res.redirect('/campgrounds/' + req.params.id);
+    });
+});
+
 module.exports = router;
