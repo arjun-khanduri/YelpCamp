367f7d82c10a1bebe55854b904cbc9b2782b02ef
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-03-09 20:54:15 +0530

    Comment Authorization

--- routes/comments.js
+++ routes/comments.js
@@ -10,6 +10,24 @@ isLoggedIn = (req, res, next) => {
     res.redirect('/login');
 }
 
+checkCommentOwnership = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if (err)
+                res.redirect('back');
+            else {
+                if (foundComment.author.id.equals(req.user._id))
+                    next();
+                else
+                    res.redirect('back');
+            }
+        });
+    }
+    else {
+        res.redirect('back');
+    }
+}
+
 router.get('/new', isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if (err)
@@ -46,7 +64,7 @@ router.post('/', isLoggedIn, (req, res) => {
 });
 
 
-router.get('/:comment_id/edit', (req, res) => {
+router.get('/:comment_id/edit', checkCommentOwnership, (req, res) => {
     Comment.findById(req.params.comment_id, (err, foundComment) => {
         if (err)
             res.redirect('back');
@@ -55,7 +73,7 @@ router.get('/:comment_id/edit', (req, res) => {
     });
 });
 
-router.put('/:comment_id', (req, res) => {
+router.put('/:comment_id', checkCommentOwnership, (req, res) => {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
         if(err)
             res.redirect('back');
@@ -64,7 +82,7 @@ router.put('/:comment_id', (req, res) => {
     })
 });
 
-router.delete('/:comment_id', (req, res) => {
+router.delete('/:comment_id', checkCommentOwnership, (req, res) => {
     Comment.findByIdAndRemove(req.params.comment_id, (err) => {
         if(err)
             res.redirect('back');
